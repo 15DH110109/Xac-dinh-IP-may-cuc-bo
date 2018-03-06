b1281e3fafb3c22ca7e37d1126c01ca3a3c0f432
dinhkhang19051997 <dinhkhang19051997@gmail.com>
2018-03-06 23:23:30 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+namespace Xac_dinh_IP_may_cuc_bo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string HostN = Dns.GetHostName();
+            Console.WriteLine("{0}", HostN);
+            IPHostEntry IP = Dns.GetHostByName(HostN);
+            int nIP = 0;
+            foreach (IPAddress ip in IP.AddressList)
+            {
+                Console.Write("IP {0}: {1}", ++nIP, ip.ToString());
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
